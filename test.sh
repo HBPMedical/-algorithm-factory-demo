5b621a2ce01c07148f0a2baf9aa59abbdd127060
Ludovic Claude <ludovic.claude54@gmail.com>
2017-08-21 14:37:18 +0200

    CircleCI: do not use sudo

--- test.sh
+++ test.sh
@@ -7,27 +7,35 @@ exit_trap () {
 trap exit_trap EXIT
 set -e
 
+if [[ $NO_SUDO || -n "$CIRCLECI" ]]; then
+  DOCKER_COMPOSE="docker-compose"
+elif groups $USER | grep &>/dev/null '\bdocker\b'; then
+  DOCKER_COMPOSE="docker-compose"
+else
+  DOCKER_COMPOSE="sudo docker-compose"
+fi
+
 echo "Woken Test 1: Get data"
-output=$(docker-compose run --rm tester ./query-data.sh -h)
+output=$($DOCKER_COMPOSE run --rm tester ./query-data.sh -h)
 
 response=$( echo "$output" | head -n 1)
 [[ "$response" == *"OK"* ]] && echo "Successful!" || { echo "Failed test 1:\n$output" 1>&2 ; exit 1; }
 
 echo "Woken Test 2: Get list of methods"
 
-output=$(docker-compose run --rm tester ./query-list-methods.sh -h)
+output=$($DOCKER_COMPOSE run --rm tester ./query-list-methods.sh -h)
 response=$( echo "$output" | head -n 1)
 [[ "$response" == *"OK"* ]] && echo "Successful!" || { echo "Failed test 2:\n$output" 1>&2 ; exit 1; }
 
 echo "Woken Test 3: Run a simple algorithm"
 
-output=$(docker-compose run --rm tester ./query-knn.sh -h)
+output=$($DOCKER_COMPOSE run --rm tester ./query-knn.sh -h)
 response=$( echo "$output" | head -n 1)
 [[ "$response" == *"OK"* ]] && echo "Successful!" || { echo "Failed test 3:\n$output" 1>&2 ; exit 1; }
 
 echo "Woken Test 4: Run a simple experiment"
 
-output=$(docker-compose run --rm tester ./query-experiment.sh -h)
+output=$($DOCKER_COMPOSE run --rm tester ./query-experiment.sh -h)
 response=$( echo "$output" | head -n 1)
 [[ "$response" == *"OK"* ]] && echo "Successful!" || { echo "Failed test 4: $response\n$output" 1>&2 ; exit 1; }
 
